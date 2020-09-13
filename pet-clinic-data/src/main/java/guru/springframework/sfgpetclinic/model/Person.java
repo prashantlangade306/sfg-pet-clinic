1f1742e724bb10c57f3ae09bd28271fb23bbe762
Prashant Langade <prashant.langade306@gmail.com>
2020-09-13 12:51:19 +1000

    added tests. Closes #49

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -16,4 +16,10 @@ public class Person extends BaseEntity{
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
